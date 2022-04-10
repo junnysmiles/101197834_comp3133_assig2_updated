185f912214c90efcb189ddd2cebaada7817687f4
Jun Yan Gan <Jun-Yan.Gan@georgebrown.ca>
2022-04-10 02:27:13 -0400

    starting to work on admin dashboard, updated login component and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdmindashboardComponent } from './admindashboard/admindashboard.component';
+import { CustomerdashboardComponent } from './customerdashboard/customerdashboard.component';
 import { ListingslistComponent } from './listingslist/listingslist.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
@@ -7,7 +9,9 @@ import { SignupComponent } from './signup/signup.component';
 const routes: Routes = [
   { path:'', component: ListingslistComponent },
   { path:'login', component: LoginComponent },
-  { path:'signup', component: SignupComponent }
+  { path:'signup', component: SignupComponent },
+  { path:'admin', component: AdmindashboardComponent},
+  { path:'customer', component: CustomerdashboardComponent}
 ];
 
 @NgModule({
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Apollo, gql } from 'apollo-angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,14 +10,35 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apolloClient: Apollo, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   username = new FormControl('', [Validators.required]);
   password = new FormControl('', [Validators.required]);
+  users = []
+  user = ""
+  pass = ""
   hide = true;
+
+  private GET_USERS = gql`
+    query{
+      getUsers{
+        username
+        password
+        type
+      }
+    }
+  `
+
+  // private LOGIN_CUSTOMER = gql`
+  //   query{
+  //     loginCustomer($un:String!, $pw:String!){
+  //       secret
+  //     }
+  //   }
+  // `
 
   getUsernameErrorMessage() {
     if (this.username.hasError('required')) {
@@ -33,4 +56,19 @@ export class LoginComponent implements OnInit {
     return this.password.hasError('password') ? 'Not a valid password' : '';
   }
 
+  // loginUser() {
+  //   this.apolloClient.query<any>({
+  //     query: this.GET_USERS,
+  //   }).subscribe(response => {
+  //     console.log(response)
+  //     console.log(response.data)
+  //     this.users = response.data?.getUsers
+
+  //     console.log(response.data?.getUsers[0])
+
+  //     if(this.user == response.data.getUsers[].username && this.pass == response.data.getUsers) {
+  //       this.router.navigate(['/admin']);
+  //     }
+  //   })
+  // }
 }
