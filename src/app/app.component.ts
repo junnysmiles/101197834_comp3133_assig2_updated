44eadfaa4e759784776af153a62a9bfda7c3c3e9
Jun Yan Gan <Jun-Yan.Gan@georgebrown.ca>
2022-04-10 17:12:48 -0400

    updated menu to hide when logged in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '101197834_comp3133_assig2_updated';
+
+  constructor(private router: Router) { }
+
+  isNavigated() {
+    const url = this.router.url
+    if(url == "/admin"){
+      return false
+    }
+    if(url == "/customer"){
+      return false
+    }
+    if(url == "/admin/create-a-listing"){
+      return false
+    }
+    return true
+  }
+
+  isLoggedIn(){
+    const url = this.router.url
+    if(url == "/admin"){
+      return true
+    }
+    if(url == "/customer"){
+      return true
+    }
+    if(url == "/admin/create-a-listing"){
+      return true
+    }
+    return false
+  }
 }
